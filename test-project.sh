57cdeda8118726183a39446c5c0afdb0b68c1494
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-28 09:10:23 -0600

    fix test-project.sh

--- test-project.sh
+++ test-project.sh
@@ -5,7 +5,7 @@ KISS_PROJECT=${KISS_PROJECT:-bad-nlp}
 
 if [ -n "$CI_OS_NAME" ]; then
     if [ ! -d projects/$KISS_PROJECT ]; then
-        (cd projects && git clone https://github.com/kiss-lang/$project)
+        (cd projects && git clone https://github.com/kiss-lang/$KISS_PROJECT)
     fi
     (cd projects/$KISS_PROJECT && lix download)
 fi
