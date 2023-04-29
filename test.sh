20912a0461170d6c8ec145c5eace57674b141d53
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-29 17:02:17 -0600

    Fix install kiss core test dependencies

--- test.sh
+++ test.sh
@@ -3,19 +3,19 @@
 KISS_TARGET=${KISS_TARGET:-$1}
 KISS_TARGET=${KISS_TARGET:-interp}
 
-# If Travis is running tests, basic dependencies need to be installed
-if [ -n "$CI_OS_NAME" ]
-then
-    lix download
-fi
-
 # Test projects with test-project.sh
 if [ -n "$KISS_PROJECT" ]
 then
     ./test-project.sh
 # Test Kiss with utest cases in kiss/src/test/cases
 else
-    git clone https://github.com/kiss-lang/kiss
+    # If CI is running tests, basic dependencies need to be installed
+    if [ -n "$CI_OS_NAME" ]
+    then
+        git clone https://github.com/kiss-lang/kiss
+        (cd kiss && lix download)
+    fi
+
     if [ ! -z "$2" ]; then
         haxe -D cases=$2 kiss/build-scripts/common-args.hxml kiss/build-scripts/common-test-args.hxml kiss/build-scripts/$KISS_TARGET/test.hxml
     else
