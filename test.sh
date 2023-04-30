b5231f4068680b0c53407255e72d48f0c125a052
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-29 18:55:07 -0600

    fix typo

--- test.sh
+++ test.sh
@@ -15,7 +15,7 @@ else
         git clone https://github.com/kiss-lang/kiss
         (cd kiss && lix download)
         if [ "$KISS_TARGET" = cpp ]; then
-            lib install haxelib:hxcpp
+            lix install haxelib:hxcpp
         elif [ "$KISS_TARGET" = nodejs ]; then
             lix install haxelib:hxnodejs
         fi
