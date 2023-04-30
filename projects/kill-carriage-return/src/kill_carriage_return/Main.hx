c2e937c290207cdb686ea395874aba697555c162
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-29 18:21:18 -0600

    remove kill-carriage-return subtree

--- projects/kill-carriage-return/src/kill_carriage_return/Main.hx
+++
@@ -1,7 +0,0 @@
-package kill_carriage_return;
-
-class Main {
-    static function main() {
-        Main_.main();
-    }
-}
--- projects/kill-carriage-return/test.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-haxelib dev kill-carriage-return ./
-(cd ../../ && haxelib run kill-carriage-return stun sh)
