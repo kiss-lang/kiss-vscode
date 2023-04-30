22184fd26c96b3f74fe6b4eb74bcb3fe608d00ab
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-04-29 18:25:40 -0600

    delete bad-nlp subtree

--- projects/bad-nlp/src/bad_nlp/Main.hx
+++
@@ -1,7 +0,0 @@
-package bad_nlp;
-
-class Main {
-    static function main() {
-        Main_.main();
-    }
-}
--- projects/bad-nlp/src/bad_nlp/Util.hx
+++
@@ -1,9 +0,0 @@
-package bad_nlp;
-
-import kiss.Prelude;
-import kiss.List;
-using StringTools;
-using hx.strings.Strings;
-
-@:build(kiss.Kiss.build())
-class Util {}
