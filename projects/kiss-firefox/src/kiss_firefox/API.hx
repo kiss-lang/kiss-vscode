06a1c87fb26668a46af0c13eed51642e86d4fea9
Nat Quayle Nelson <natquaylenelson@gmail.com>
2023-05-05 14:58:37 -0600

    delete kiss-firefox subtree

--- projects/kiss-firefox/src/kiss_firefox/API.hx
+++
@@ -1,7 +0,0 @@
-package kiss_firefox;
-
-@:native("")
-extern class API {
-    @:native("browser")
-    static var browser:webextension_polyfill.Browser;
-}
--- projects/kiss-firefox/template/test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-if [ ! -d node_modules ]; then
-    $(haxelib libpath kiss)/build-scripts/dts2hx-externs/regenerate.sh
-fi
-
-haxe -D test build.hxml
